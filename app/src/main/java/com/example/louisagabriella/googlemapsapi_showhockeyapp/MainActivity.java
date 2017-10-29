64fef9f334da9108679109c35b664766e8f542d0
louisa gabriella <louisa_gabriella_24rpl@student.smktelkom-mlg.sch.id>
2017-10-29 04:39:40 -0700

    Add Show Hockey Revisi Finish

--- app/src/main/java/com/example/louisagabriella/googlemapsapi_showhockeyapp/MainActivity.java
+++ app/src/main/java/com/example/louisagabriella/googlemapsapi_showhockeyapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.louisagabriella.googlemapsapi_showhockeyapp;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,6 +18,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
@@ -44,28 +53,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         Tulungagung = new MarkerOptions()
                 .position(new LatLng(-8.091221, 111.964173))
-                .title("Tulungagung")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_place_black_24dp));
+                .icon(vectorToBitmap(R.drawable.ic_place_black_24dp, Color.parseColor("#A4C639")))
+                .title("Tulungagung");
 
         Kediri = new MarkerOptions()
                 .position(new LatLng(-7.848016, 112.017829))
-                .title("Kediri")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_place_black_24dp));
+                .icon(vectorToBitmap(R.drawable.ic_place_black_24dp, Color.parseColor("#A4C639")))
+                .title("Kediri");
 
         Pare = new MarkerOptions()
                 .position(new LatLng(-7.765512, 112.197899))
-                .title("Pare")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_place_black_24dp));
+                .icon(vectorToBitmap(R.drawable.ic_place_black_24dp, Color.parseColor("#A4C639")))
+                .title("Pare");
 
         Wlingi = new MarkerOptions()
                 .position(new LatLng(-8.129380, 112.320999))
-                .title("Wlingi")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_place_black_24dp));
+                .icon(vectorToBitmap(R.drawable.ic_place_black_24dp, Color.parseColor("#A4C639")))
+                .title("Wlingi");
 
         Blitar = new MarkerOptions()
                 .position(new LatLng(-8.095463, 112.160906))
-                .title("Blitar")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_place_black_24dp));
+                .icon(vectorToBitmap(R.drawable.ic_place_black_24dp, Color.parseColor("#A4C639")))
+                .title("Blitar");
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -74,19 +83,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -96,7 +100,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap map) {
-        //MapsInitializer.initialize(getApplicationContext());
         mapReady = true;
         m_map = map;
         m_map.addMarker(Tulungagung);
@@ -109,5 +112,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private void flyTo(CameraPosition target) {
         m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+    }
+
+    private BitmapDescriptor vectorToBitmap(@DrawableRes int id, @ColorInt int color) {
+        Drawable vectorDrawable = ResourcesCompat.getDrawable(getResources(), id, null);
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        DrawableCompat.setTint(vectorDrawable, color);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
